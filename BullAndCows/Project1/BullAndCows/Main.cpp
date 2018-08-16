32c952c85d6f8b1d899f09ccc5ee14587374849f
Genjo <insidia92@gmail.com>
2018-08-16 15:26:35 +0300

    Learn method getline and how it works

--- BullAndCows/Project1/BullAndCows/Main.cpp
+++ BullAndCows/Project1/BullAndCows/Main.cpp
@@ -7,13 +7,20 @@ int main()
 	//intorduce the game
 	constexpr int WORD_LENGTH = 9;
 	cout << "Welcome to BullCow Game,fun game\n";
-	cout << "Can you guess the  " << WORD_LENGTH << " lettter isogram I'm thinking of?\n";
+	cout << "Can you guess the  " << WORD_LENGTH << " lettter isogram I'm thinking of?\n\n";
 	
 	
 	//get guess from the user
 	string Guess = "";
 	cout << "Input your guess: ";
-	cin >> Guess;
+	getline(cin, Guess);
+	//repeat the guess to him
+	cout << "Your guess is " << Guess << "\n";
+	
+	//get guess from the user
+	cout << "Input your guess: ";
+	getline(cin, Guess);
+	//repeat the guess to him
 	cout << "Your guess is " << Guess << "\n";
 	return 0;
 }
