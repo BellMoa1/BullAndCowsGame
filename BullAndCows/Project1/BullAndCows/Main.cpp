aa631b0da2cfe7ccb23a2f507afacd13d7c9fe38
Genjo <insidia92@gmail.com>
2018-08-16 15:45:17 +0300

    In this lecture we learn how to use functions and prototypes

--- BullAndCows/Project1/BullAndCows/Main.cpp
+++ BullAndCows/Project1/BullAndCows/Main.cpp
@@ -2,25 +2,34 @@
 #include <string>
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+
 int main()
+{
+	PrintIntro();
+    GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
+	
+	return 0;
+}
+
+void PrintIntro()
 {
 	//intorduce the game
 	constexpr int WORD_LENGTH = 9;
 	cout << "Welcome to BullCow Game,fun game\n";
 	cout << "Can you guess the  " << WORD_LENGTH << " lettter isogram I'm thinking of?\n\n";
-	
-	
+	return;
+}
+
+string GetGuessAndPrintBack()
+{
 	//get guess from the user
 	string Guess = "";
 	cout << "Input your guess: ";
 	getline(cin, Guess);
-	//repeat the guess to him
-	cout << "Your guess is " << Guess << "\n";
-	
-	//get guess from the user
-	cout << "Input your guess: ";
-	getline(cin, Guess);
-	//repeat the guess to him
-	cout << "Your guess is " << Guess << "\n";
-	return 0;
+	cout << "Your Guess is " << Guess << "\n";
+	return Guess;
 }
