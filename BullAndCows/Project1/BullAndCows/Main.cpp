8420e3f0b17d2bc1235969772cacc575394388e1
Genjo <insidia92@gmail.com>
2018-08-17 15:45:35 +0300

    Created a new function PlayGame and replace our loop in this function

--- BullAndCows/Project1/BullAndCows/Main.cpp
+++ BullAndCows/Project1/BullAndCows/Main.cpp
@@ -1,40 +1,52 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 void PrintIntro();
-string GetGuessAndPrintBack();
+void PlayGame();
+string GetGuess();
 
-
+//the entry point for our app
 int main()
 {
-	constexpr int NUMBERS_OF_TURNS = 5;
+	
 	PrintIntro();
+	PlayGame();
 	
-	//loop for the numbers of turns asking guess
-	for (size_t count = 0; count < NUMBERS_OF_TURNS; count++)
-	{
-		GetGuessAndPrintBack();
-	}
-	
-	return 0;
+	return 0;//exit the app
 }
 
+
+//intorduce the game
 void PrintIntro()
 {
-	//intorduce the game
 	constexpr int WORD_LENGTH = 9;
 	cout << "Welcome to BullCow Game,fun game\n";
 	cout << "Can you guess the  " << WORD_LENGTH << " lettter isogram I'm thinking of?\n\n";
 	return;
 }
 
-string GetGuessAndPrintBack()
+
+void PlayGame()
 {
-	//get guess from the user
+	string Guess = "";
+	constexpr int NUMBERS_OF_TURNS = 5;
+	//loop for the numbers of turns asking guess
+	for (size_t count = 0; count < NUMBERS_OF_TURNS; count++)
+	{
+		Guess = GetGuess();
+		cout << "Your Guess is " << Guess << "\n";
+	}
+}
+
+
+//get guess from the user
+string GetGuess()
+{
 	string Guess = "";
 	cout << "Input your guess: ";
 	getline(cin, Guess);
-	cout << "Your Guess is " << Guess << "\n";
+	
 	return Guess;
 }
